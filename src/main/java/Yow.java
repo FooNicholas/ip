9af9b0a714157a3c76c275e9f10ed650e8f3e595
Nicholas Foo <foosekwoon@gmail.com>
2025-01-23 15:03:29 +0800

    Implemented Echo

--- src/main/java/Yow.java
+++ src/main/java/Yow.java
@@ -1,23 +1,46 @@
+import java.util.Scanner;
+
 public class Yow {
 
     public void run() {
+        Boolean stopCommand = false;
+        Scanner scanner = new Scanner(System.in);
+
         startChat();
+
+        while (!stopCommand) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                stopCommand = true;
+            } else {
+                echo(userInput);
+            }
+        }
+
         endChat();
+        scanner.close();
     }
     public void splitLine() {
         System.out.println("____________________________________________________________");
     }
 
+    public void prettyPrint(String text) {
+        splitLine();
+        System.out.println(text);
+        splitLine();
+    }
     public void startChat() {
-        splitLine();
-        System.out.println("Hello! I'm Yow\n"
-                        + "What can I do for you?");
-        splitLine();
+        prettyPrint("Hello! I'm Yow\n"
+                        + "What can I do for you yow?");
     }
 
     public void endChat() {
-        System.out.println("Bye. Hope to see you again soon!");
-        splitLine();
+        prettyPrint("Bye. Hope to see you again soon yow!");
+    }
+
+    public void echo(String input) {
+        prettyPrint(input);
     }
 
     public static void main(String[] args) {
